02a96b34cf34758345bd411896f7daccb770304d
Nikola M <nikolamilovic2001@gmail.com>
2020-08-23 17:18:10 +0200

    added options and prices to orders

--- core/src/main/java/com/nikolam/core/model/OrderItem.kt
+++ core/src/main/java/com/nikolam/core/model/OrderItem.kt
@@ -1,3 +1,3 @@
 package com.nikolam.core.model
 
-data class OrderItem(var name : String = "", var price : Int = 0, val itemID : String = "")
+data class OrderItem(var name : String = "" ,var text : String = "", var price : Int = 0, val itemID : String = "", var amount : Int = 0)
--- core/src/main/java/com/nikolam/core/order/OrderPersistant.kt
+++ core/src/main/java/com/nikolam/core/order/OrderPersistant.kt
@@ -10,11 +10,26 @@ class OrderPersistant () {
     fun addOrderItem(menuItem: MenuItem){
 
         menuItem.prices.forEach {price ->
+            var added = false
+            val fullName = menuItem.name + " " + price.option
             if (price.amount > 0) {
-                val orderItem = OrderItem()
-                orderItem.price = price.price * price.amount
-                orderItem.name = menuItem.name + " " + price.option + "       X " + price.amount.toString() + "     " + orderItem.price.toString() + "RSD"
-                orderItems.add(orderItem)
+                for (i in orderItems.indices){
+                    if (orderItems[i].name == fullName) {
+                        orderItems[i].price += price.price * price.amount
+                        orderItems[i].amount += price.amount
+                        orderItems[i].text = fullName + "       X " + orderItems[i].amount.toString() + "     " + orderItems[i].price.toString() + "RSD"
+                        added = true
+                    }
+                }
+                if (!added) {
+                    val orderItem = OrderItem()
+                    orderItem.name = fullName
+                    orderItem.amount = price.amount
+                    orderItem.price = price.price * price.amount
+                    orderItem.text =
+                        fullName + "       X " + orderItem.amount.toString() + "     " + orderItem.price.toString() + "RSD"
+                    orderItems.add(orderItem)
+                }
             }
         }
     }
